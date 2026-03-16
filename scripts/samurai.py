7ba17b6674d887c8a046613f45607241182c24db
gitmskhl <gglmskhl@gmail.com>
2026-03-17 00:22:12 +0300

    fix sync problem

--- scripts/samurai.py
+++ scripts/samurai.py
@@ -20,7 +20,7 @@ class Samurai(wizard.Wizard):
         print(self.y_spead)
         None
     def render_boundbox(self):
-        cent,bb=self.center_of_boundbox()
+        cent,bb = self.center_of_boundbox()
         pygame.draw.rect(pygame.display.get_surface(),(255,0,0),(cent[0]-bb.width//10+(self.move_right-self.move_left)*10,cent[1]-bb.height//20+15,bb.width//4.7,bb.height//4+30),1)
-        boundbox=pygame.Rect(cent[0]-bb.width//10+(self.move_right-self.move_left)*10,cent[1]-bb.height//20+15,bb.width//4.7,bb.height//4+30)
+        boundbox = pygame.Rect(cent[0]-bb.width//10+(self.move_right-self.move_left)*10,cent[1]-bb.height//20+15,bb.width//4.7,bb.height//4+30)
         return(boundbox)
